90bd4ec7c3e20928ff86f1bf4f373ff9cc36b87c
Eduardo-Antoniassi-Lobato <eduardoantlobato@gmail.com>
2023-03-21 20:44:19 +0000

    add turtle module to run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from turtle import Turtle, Screen
 logo = """
  ______     ______   ______     __  __           ______           ______     __  __     ______     ______   ______     __  __    
 /\  ___\   /\__  _\ /\  ___\   /\ \_\ \         /\  __ \         /\  ___\   /\ \/ /    /\  ___\   /\__  _\ /\  ___\   /\ \_\ \   
@@ -12,3 +13,4 @@ print("Use the arrows to sketch.")
 print("Press 'c' to clear the screen and start drawing again.")
 print("Press 'space' or click over the screen to end the game.")
 input("Press 'enter' to start.")
+
