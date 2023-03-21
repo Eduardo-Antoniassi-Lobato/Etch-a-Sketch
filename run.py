24f74372047690b6229fb451600b07af380aad02
Eduardo-Antoniassi-Lobato <eduardoantlobato@gmail.com>
2023-03-21 20:52:52 +0000

    starts GUI and sketch pen to the game

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+# import module turtle
 from turtle import Turtle, Screen
+
 logo = """
  ______     ______   ______     __  __           ______           ______     __  __     ______     ______   ______     __  __    
 /\  ___\   /\__  _\ /\  ___\   /\ \_\ \         /\  __ \         /\  ___\   /\ \/ /    /\  ___\   /\__  _\ /\  ___\   /\ \_\ \   
@@ -7,10 +9,18 @@ logo = """
   \/_____/     \/_/   \/_____/   \/_/\/_/         \/_/\/_/         \/_____/   \/_/\/_/   \/_____/     \/_/   \/_____/   \/_/\/_/ 
                                                                                                                                  
 """
+
+# Intro Game with instructions
 print(logo)
 print("Welcome to Etch-a-Sketch !")
 print("Use the arrows to sketch.")
 print("Press 'c' to clear the screen and start drawing again.")
 print("Press 'space' or click over the screen to end the game.")
 input("Press 'enter' to start.")
+
+# starts GUI and sketch pen
+sketch_pen = Turtle() 
+screen = Screen()
+
+
 
